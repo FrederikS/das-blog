e2f3cec5319e90bcd763cdd56068878b7a8d4aeb
frederik.steffen <frederik.steffen@gmail.com>
2016-05-06 11:36:42 +0200

    added CORS support for post-api

--- src/main/scala/de/fsteffen/blog/app/Routes.scala
+++ src/main/scala/de/fsteffen/blog/app/Routes.scala
@@ -2,9 +2,12 @@ package de.fsteffen.blog.app
 
 import akka.http.scaladsl.server.Directives._
 import de.fsteffen.blog.api.ElasticBasedPostsApiComponent
+import de.fsteffen.blog.http.CorsSupport
 
-trait Routes extends ElasticBasedPostsApiComponent {
+trait Routes extends ElasticBasedPostsApiComponent with CorsSupport {
   val routes = pathPrefix("v1") {
-    postRoutes
+    corsHandler {
+      postRoutes
+    }
   }
 }
