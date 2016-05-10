fb84568321629bc20af69dabc8512f6066ff7694
frederik.steffen <frederik.steffen@gmail.com>
2016-05-10 22:13:38 +0200

    activated rejection-handler for cors-requests allowing generic requests

--- src/main/scala/de/fsteffen/blog/app/Routes.scala
+++ src/main/scala/de/fsteffen/blog/app/Routes.scala
@@ -3,6 +3,7 @@ package de.fsteffen.blog.app
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model.headers.{HttpOrigin, HttpOriginRange}
 import akka.http.scaladsl.server.Directives._
+import ch.megard.akka.http.cors.CorsDirectives._
 import ch.megard.akka.http.cors.{CorsDirectives, CorsSettings, HttpHeaderRange}
 import com.typesafe.config.ConfigFactory
 
@@ -12,15 +13,17 @@ import de.fsteffen.blog.api.ElasticBasedPostsApiComponent
 trait Routes extends ElasticBasedPostsApiComponent with CorsDirectives {
   private val config = ConfigFactory.load()
   private val settings = CorsSettings.defaultSettings.copy(
-    allowGenericHttpRequests = false,
+    allowGenericHttpRequests = true,
     allowedOrigins = HttpOriginRange(HttpOrigin(config.getString("cors.allowed-origin"))),
     allowedMethods = Seq(GET, POST, PUT, DELETE, OPTIONS),
     allowedHeaders = HttpHeaderRange("accept", "content-type")
   )
 
   val routes = pathPrefix("v1") {
-    cors(settings) {
-      postRoutes
+    handleRejections(corsRejectionHandler) {
+      cors(settings) {
+        postRoutes
+      }
     }
   }
 }
